f1b93dd98aa438ba5f74ccab4782f6c875e10c37
unknown <paulohnq55@outlook.com>
2021-08-17 18:22:13 -0300

    finalizado

---
+++ Arrays/ContaCorrente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class ContaCorrente
+    {
+        public string Titular { get; set; }
+        public int NumeroConta { get;}
+        public int NumeroAgencia { get; }
+
+        public ContaCorrente()
+        {
+
+        }
+        public ContaCorrente(string titular, int numeroConta, int numeroAgencia)
+        {
+            Titular = titular;
+            NumeroConta = numeroConta;
+            NumeroAgencia = numeroAgencia;
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+        }
+
+
+
+
+
+        static void TestaArray()
+        {
+            //criando array
+            int[] idades = new int[5];
+
+            idades[0] = 20;
+            idades[1] = 18;
+            idades[2] = 21;
+            idades[3] = 20;
+            idades[4] = 16;
+            //variavel para acumular soma de idades
+            int acumulador = 0;
+            int media;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                int idade = idades[i];
+                acumulador = acumulador + idade;
+            }
+
+            media = acumulador / idades.Length; //---->Length=tamanho;
+            Console.WriteLine(media);
+
+        }
+        static void InicializandoArrayDeObjetos()
+        {
+            //inicialização de arrays 
+            ContaCorrente[] contas = new ContaCorrente[] {
+
+                 new ContaCorrente("PAULO",1160397,1234),
+                 new ContaCorrente("MARINA", 1158968, 1234),
+                 new ContaCorrente("MARIANA", 87899632, 1234)
+            };
+
+
+            for (int i = 0; i < contas.Length; i++)
+            {
+                Console.WriteLine($"Conta no indice [{i}] Titular:{contas[i].Titular} Conta: {contas[i].NumeroConta} Agência: {contas[i].NumeroAgencia}");
+
+            }
+        }
+    }
+}
